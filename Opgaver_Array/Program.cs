96bcc1111e5b1f7d5b89b1b12b498af20f3164e9
Thomas Jensen <admin@AR-114-01>
2018-03-02 09:34:02 +0100

    tilføjelse af struct og array opgaver

---
+++ Opgaver_Array/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgaver_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] månedsLøn = { 1000, 2000, 49890, 5000, 1000 };
+
+            int printeVærdien;
+            double liggeTalSammen = 0;
+           
+
+            //Printe selve værdien
+            for (int i = 0; i < månedsLøn.Length; i++)
+            {
+                printeVærdien = månedsLøn[i];
+
+                Console.WriteLine("Her printer jeg den aktuelle værdi");
+                Console.WriteLine(printeVærdien);
+
+               
+                Console.ReadKey();
+            }
+
+            //Printe tallene bliver lagt sammen
+
+            double sum = 0;
+
+            for (int i = 0; i < månedsLøn.Length; i++)
+            {
+                liggeTalSammen += månedsLøn[i];
+
+                Console.WriteLine("Her printer jeg adderingen af de forskellige værdier");
+                Console.WriteLine(liggeTalSammen);
+              
+                Console.ReadKey();
+            }
+
+            sum = liggeTalSammen / månedsLøn.Count(); //Alternativ length
+
+            Console.WriteLine("Her printer jeg gns");
+
+            //Formatering af tallet
+            Console.WriteLine(sum.ToString("N2"));
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Opgaver_struct/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgaver_struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person k1 = new Person();
+            k1.navn = "a";
+
+            Person k2 = new Person();
+            k2.navn = "b";
+
+            Console.WriteLine(k1.navn); //Printer a -Fordi det er de sat til i starten
+            Console.WriteLine(k2.navn); //Printer b -Fordi det er de sat til i starten
+
+            k1 = k2;
+
+
+            Console.WriteLine(k1.navn); //Printer b - Fordi vi sætter k1 til at være = k2
+            Console.WriteLine(k2.navn); //Printer b - Fordi den er uændret
+
+            k1.navn = "c";
+
+
+            Console.WriteLine(k1.navn); //Printer c - Fordi nu referer vi k1.navn til at skal være = c
+            Console.WriteLine(k2.navn); //Printer b
+
+
+            Console.ReadKey();
+
+            emps();
+        }
+
+        static void emps()
+        {
+            Console.WriteLine("Ændring til class istedet for struct");
+            Employee k1 = new Employee();
+            k1.navn = "a";
+
+            Employee k2 = new Employee();
+            k2.navn = "b";
+
+            Console.WriteLine(k1.navn); //Printer a - Fordi det er de sat til i starten
+            Console.WriteLine(k2.navn); //Printer b - Fordi det er de sat til i starten
+
+            k1 = k2;
+
+
+            Console.WriteLine(k1.navn); //Printer b - Fordi vi sætter k1 til at være = k2 classen - Så k1 bliver droppet
+            Console.WriteLine(k2.navn); //Printer b - Fordi den er uændret
+
+            k1.navn = "c";
+
+
+            Console.WriteLine(k1.navn); //Printer c - Fordi nu referer vi k1.navn til at skal være = c
+            Console.WriteLine(k2.navn); //Printer c - Fordi vi nu referer til et heap k2, da vi ovenoversætte k1 = k2, så er det ligemeget hvad vi ændrer, vi ændre værdien for det heap som vi kun har.
+                                        //Dvs k1.navn eller k2.navn vil ændre den samme værdi på det ene heap
+
+
+            Console.ReadKey();
+        }
+
+        class Employee
+        {
+            public string navn;
+        }
+        struct Person
+        {
+            public string navn;
+        }
+    }
+}
