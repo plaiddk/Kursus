1578e6cd8fcec4516a5d07bd0667f51f7609dfed
Thomas Jensen <admin@AR-114-01>
2018-03-02 10:41:38 +0100

    ændringer

--- Opgaver_løkke/Program.cs
+++ Opgaver_løkke/Program.cs
@@ -10,12 +10,14 @@ namespace Opgaver_løkke
     {
         static void Main(string[] args)
         {
-
+            //Loop fra 1 til 11 - Da det er 10 tabellen vi skal bruge, så 0 skal selvfølgelig ikke med og 10 er max
             for (int i = 1; i < 11; i++)
             {
-               
+               //Gange dem op så vi får tabellen
                 for (int j =1; j < 11; j++)
                 {
+
+                    //Hvis værdien er = eller over 50 så skriv gul tekst eller hvid
                      if (i * j >= 50)
                         {
                             Console.ForegroundColor = ConsoleColor.Yellow;
@@ -25,8 +27,12 @@ namespace Opgaver_løkke
                         {
                             Console.ForegroundColor = ConsoleColor.White;
                         }
+
+                        //Ved afslutning af hver række horisontal lav et mellemrum med tabulartor tegnet
                         Console.Write(i * j + "\t");
                 }
+
+                //Ved afslutning af hver række ned af 1 til 10 afslut med newline
                 Console.Write("\n");
 
             }
